9c35c64deb5d6e886b89ff5cc8e7d083a20a1f94
Agata Brok <agatbr@gmail.com>
2019-08-19 17:40:58 +0100

    Update desktop.

--- desktop.js
+++ desktop.js
@@ -4,13 +4,18 @@ _xyz({
 		_xyz.mapview.create({
 			target: document.getElementById('_map'),
 			scrollWheelZoom: true,
-			zoomControl: true,
-			btn: {
-				Locate: document.getElementById('btnLocate')
-			}
+			zoomControl: true
 		});
 
 		const layer_dropdown_options = [];
+
+		const core_layer_key = 'Wellbeing Index';
+		const core_layer_themes = _xyz.layers.list[core_layer_key].style.themes;
+		const core_layer_themes_options;
+
+		Object.keys(_xyz.layers.list[core_layer_key].style.themes).map(key => {
+			core_layer_themes_options.push(key);
+		});
 
 		Object.values(_xyz.layers.list).map(layer => {
 			if(layer.group && layer.group === 'Locations'){
@@ -21,10 +26,17 @@ _xyz({
 			}
 		});
 
+		const core_layer_themes_dropdown = _xyz.utils.dropdownCustom({
+			entries: core_layer_themes_options,
+			callback: e => {
+				console.log(e.target);
+			}
+		});
+
 		const layer_dropdown = _xyz.utils.dropdownCustom({
 			entries: layer_dropdown_options,
 			callback: e => {
-				
+
 				Object.values(_xyz.layers.list).map(layer => {
 					if(layer.group && layer.group === 'Locations'){
 						layer.display = layer.key === e.target.dataset.field  ? true : false;
@@ -36,6 +48,7 @@ _xyz({
 		});
 
 		document.getElementById('layer_dropdown').appendChild(layer_dropdown);
+		document.getElementById('core_layer_themes_dropdown').appendChild(core_layer_themes_dropdown);
 
 	}
 });
