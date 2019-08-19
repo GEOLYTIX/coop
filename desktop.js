1970ae253942145d87e2629c6f1cd1860da1f80a
Agata Brok <agatbr@gmail.com>
2019-08-19 14:59:44 +0100

    Map initialized.

--- desktop.js
+++ desktop.js
@@ -1,1 +1,19 @@
 console.log('Hello World');
+
+_xyz({
+	host: document.head.dataset.dir,
+	callback: _xyz => {
+		_xyz.mapview.create({
+			target: document.getElementById('Map'),
+			scrollWheelZoom: true,
+			zoomControl: true,
+			btn: {
+				Locate: document.getElementById('btnLocate')
+			}
+		});
+
+		Object.values(_xyz.layers.list).map(layer => {
+			console.log(layer);
+		});
+	}
+});
