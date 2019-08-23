8a8f1bb641e49044752bd770817954cd9e8e71a5
Agata Brok <cityremade@users.noreply.github.com>
2019-08-23 17:04:14 +0200

    Update desktop.js.

--- desktop.js
+++ desktop.js
@@ -7,11 +7,38 @@ _xyz({
 			zoomControl: true
 		});
 
-		const layer_dropdown_options = [];
+		// to apply on search results
+		_xyz.gazetteer.style = {
+			stroke: true,
+            color: "#6E6875",
+            weight: 2,
+            fillColor: "#95A3A4",
+            fillOpacity: 0.2,
+            fill: true
+        };
 
-		const core_layer_key = 'Wellbeing Index';
+        // to apply on the core layer
+        const core_layer_style = {
+        	stroke: true,
+        	color: '#FFF',
+            weight: 2,
+            fillColor: '#FFF',
+            fillOpacity: 0.2,
+            fill: true
+        }
+
+        // get core layer properties
+        const core_layer_key = 'Wellbeing Index';
 		const core_layer = _xyz.layers.list[core_layer_key];
 		const core_layer_themes = _xyz.layers.list[core_layer_key].style.themes;
+
+		// show core layer and make legend
+		core_layer.view();
+		makeLegend(core_layer);
+
+
+		// make dropdown options
+		const layer_dropdown_options = [];
 		const core_layer_themes_options = [];
 
 		Object.keys(core_layer.style.themes).map(key => {
@@ -20,74 +47,149 @@ _xyz({
 
 		Object.values(_xyz.layers.list).map(layer => {
 			if(layer.group && layer.group === 'Locations'){
-				
-				layer_dropdown_options.push({
-					[layer.key]: layer.name || layer.key
-				});
+				layer_dropdown_options.push({ [layer.key]: layer.name || layer.key });
 			}
 		});
 
+		// create themes dropdown
 		const core_layer_themes_dropdown = _xyz.utils.dropdownCustom({
 			entries: core_layer_themes_options,
 			singleSelect: true,
 			selectedIndex: 0,
 			callback: e => {
-				
 				e.stopPropagation();
-
 				core_layer_themes_dropdown.querySelector('.head').textContent = e.target.textContent;
-				
-				let new_theme = core_layer.style.themes[e.target.dataset.field];
-
-				let style = Object.assign({}, core_layer.style.default);
-
-				let new_style = function(){
-					if (new_theme.type === 'categorized') {
-						return Object.assign({}, style, new_theme.cat[core_layer.mvt_fields[new_theme.field]] || {});
-					}
-
-					if (new_theme.type === 'graduated') {
-
-						new_theme.cat_arr = Object.entries(new_theme.cat).sort((a, b) => parseFloat(a[0]) - parseFloat(b[0]));
-						new_theme.cat_style = {};
-
-						for (let i = 0; i < new_theme.cat_arr.length; i++) {
-
-							if (!core_layer.mvt_fields[new_theme.field]) return style;
-							if (parseFloat(core_layer.mvt_fields[new_theme.field]) < parseFloat(new_theme.cat_arr[i][0])) break;
-
-							new_theme.cat_style = new_theme.cat_arr[i][1];
-						}
-
-						return Object.assign({}, style, new_theme.cat_style);
-					}
-				}();
-
-				core_layer.style.theme = new_theme;
+				core_layer.style.theme = core_layer.style.themes[e.target.dataset.field];
 				core_layer.loaded = false;
-				// apply theme
 				core_layer.get();
-			}
+				core_layer.view.update();
+				makeLegend(core_layer);
+			}		
 		});
 
-		const layer_dropdown = _xyz.utils.dropdownCustom({
-			entries: layer_dropdown_options,
-			callback: e => {
-
-				Object.values(_xyz.layers.list).map(layer => {
-					if(layer.group && layer.group === 'Locations'){
-						layer.display = layer.key === e.target.dataset.field  ? true : false;
-						console.log('do soemthing with this layer ' + layer.key);
-						//layer.get();
-					}
-				});
-				
-			}
-		});
-
-
-		document.getElementById('layer_dropdown').appendChild(layer_dropdown);
+		// add theme dropdown to DOM
 		document.getElementById('core_layer_themes_dropdown').appendChild(core_layer_themes_dropdown);
 
+
+		// intialize gazetteer
+		_xyz.gazetteer.init({
+			target: document.getElementById('layer_search'),
+			callback: () => {} // dummy callback placeholder if needed
+		});
+
+		// remove selected feature from map when popup is closed
+		_xyz.map.on('popupclose', () => {
+			if(_xyz.locations.current) _xyz.locations.current.remove();
+		});
+
+		// select feature
+		_xyz.locations.select = location => {
+			location.layer === core_layer_key ? selectFromCoreLayer(location) : selectArea(location);
+		}
+
+		// make legend obvs
+		function makeLegend(layer){
+			document.getElementById('xyz_legend').innerHTML = '';
+			if (layer.style.theme) {
+				document.getElementById('xyz_legend').appendChild(_xyz.utils.wire()`<div>${layer.name || ''}`);
+				document.getElementById('xyz_legend').appendChild(_xyz.utils.wire()`<small>${layer.style.theme.label || ''}`);
+				document.getElementById('xyz_legend').appendChild(layer.style.legend);
+			}
+		}
+
+		function selectArea(location){ // selects area from gazetteer
+
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('GET',
+				_xyz.host + '/api/location/select/id?' +
+				_xyz.utils.paramString({
+					locale: _xyz.workspace.locale.key,
+					layer: location.layer,
+					table: location.table,
+					id: location.id,
+					token: _xyz.token
+				}));
+
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.responseType = 'json';
+
+			let current_area = document.getElementById('current_area');
+
+			xhr.onload = e => {
+
+				if (e.target.status !== 200) return;
+
+				// show headers
+				let headers = document.querySelectorAll('div div.headers'); 
+				for(let i = 0; i < headers.length; i++){
+					headers[i].style.display = 'block';
+				}
+
+				location.infoj = e.target.response.infoj;
+				location.geometry = e.target.response.geomj;
+
+				const _location = _xyz.locations.location(location); // make location
+				_xyz.gazetteer.current_location = _location; // make it current
+
+				// Create location view.
+				_location.view();
+				_location.draw(_xyz.gazetteer.style);
+				_location.flyTo();
+
+				current_area.classList.add("shaded");
+				current_area.appendChild(_location.view.node);
+			}
+
+			if(_xyz.gazetteer.current_location) { // deselect previous feature from gazetteer
+				_xyz.gazetteer.current_location = null;
+				let tables = document.querySelector('.grid.tables');
+				tables.innerHTML = '';
+				current_area.classList.remove("shaded");
+				current_area.innerHTML = '';
+			}
+			
+			xhr.send();
+		}
+
+		function selectFromCoreLayer(location){ // select feature from the core layer
+
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('GET',
+				_xyz.host + '/api/location/select/id?' +
+				_xyz.utils.paramString({
+					locale: _xyz.workspace.locale.key,
+					layer: location.layer,
+					table: location.table,
+					id: location.id,
+					token: _xyz.token
+				}));
+
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.responseType = 'json';
+
+			xhr.onload = e => {
+
+				if (e.target.status !== 200) return;
+
+				location.infoj = e.target.response.infoj;
+				location.geometry = e.target.response.geomj;
+
+				const _location = _xyz.locations.location(location); // make location
+				_xyz.locations.current = _location; // make it current
+
+				// Create location view.
+				_location.view();
+				_location.draw(core_layer_style);
+
+				_xyz.mapview.popup({ // make popup
+					latlng: [_location.marker[1], _location.marker[0]],
+					content: _location.view.node,
+					closeButton: true
+				});
+			}
+			xhr.send();
+		}
 	}
 });
