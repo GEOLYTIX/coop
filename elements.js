23c49145f28efec030c42a7e11e23ab352a04d60
Agata Brok <agatbr@gmail.com>
2019-11-13 11:27:15 +0000

    Updated dropdowns.

--- elements.js
+++ elements.js
@@ -19,9 +19,7 @@ function makeLegend(_xyz, layer) { // make legend obvs
 function createRegionList(_xyz) { // create region dropdown
 
     // selectors
-    const dropdown_1_id = 'xyz_locale_dropdown_1',
-          dropdown_2_id = 'xyz_locale_dropdown_2',
-          dropdown_3_id = 'xyz_locale_dropdown_3';
+    const dropdown_1_id = 'xyz_locale_dropdown_1';
 
     document.getElementById(dropdown_1_id).innerHTML = '';
     //document.getElementById(dropdown_2_id).innerHTML = '';
@@ -57,212 +55,23 @@ function createRegionList(_xyz) { // create region dropdown
                 table: "Overview",
                 label: "lad_name",
                 placeholder: "Select Local Authority District",
-                target_id: 'xyz_locale_dropdown_2' //dropdown_2_id
+                target_id: 'xyz_locale_dropdown_2'
             });
 
-            /*createSecondaryList(e, _xyz, {
+            createSecondaryList(e, _xyz, {
                 layer: "Constituencies",
                 table: "Overview",
                 label: "constituency_name",
                 placeholder: "Select Constituency",
-                target_id: dropdown_3_id
-            });*/
-
-            //createLADList(_xyz);
-
-            /*let layer = _xyz.layers.list["Local Authority"];
-
-            layer.filter = {};
-            layer.filter.current = {};
-            layer.filter.current["regioncode"] = {};
-            layer.filter.current["regioncode"].match = e.target.dataset.field;
-
-            const _xhr = new XMLHttpRequest();
-
-            _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
-                locale: _xyz.workspace.locale.key,
-                layer: "Local Authority",
-                table: "Overview",
-                orderby: "lad_name",
-                filter: JSON.stringify(layer.filter.current),
-                token: _xyz.token
-            }));
-
-            _xhr.setRequestHeader('Content-Type', 'application/json');
-            _xhr.responseType = 'json';
-
-            _xhr.onload = e => {
-
-                if (e.target.status !== 200) return;
-
-                let entries = [];
-
-                e.target.response.forEach(each => {
-                    entries.push({
-                        [each.qid]: each.lad_name
-                    });
-                });
-
-                const locale_dropdown_2 = _xyz.utils.dropdownCustom({
-                    entries: entries,
-                    singleSelect: true,
-                    placeholder: 'Select Local Authority District',
-                    callback: e => {
-
-                        e.stopPropagation();
-
-                        document.querySelector('.gazetteer input').value = '';
-
-                        selectArea(_xyz, {
-                            layer: layer.key,
-                            table: layer.table,
-                            id: e.target.dataset.field
-                        });
-
-                        locale_dropdown_2.querySelector('.head').textContent = e.target.textContent;
-                    }
-                });
-
-                document.getElementById(dropdown_2_id).appendChild(locale_dropdown_2);
-
-            }
-            _xhr.send();*/
+                target_id: 'xyz_locale_dropdown_3'
+            });
         }
     });
 
     return locale_dropdown_1;
 }
 
-function createLADList(_xyz){
-
-    let layer = _xyz.layers.list["Local Authority"];
-
-    layer.filter = {};
-    layer.filter.current = {};
-    layer.filter.current["regioncode"] = {};
-    layer.filter.current["regioncode"].match = e.target.dataset.field;
-
-    const _xhr = new XMLHttpRequest();
-
-    _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
-        locale: _xyz.workspace.locale.key,
-        layer: "Local Authority",
-        table: "Overview",
-        orderby: "lad_name",
-        filter: JSON.stringify(layer.filter.current),
-        token: _xyz.token
-    }));
-
-    _xhr.setRequestHeader('Content-Type', 'application/json');
-    _xhr.responseType = 'json';
-
-    _xyz.onload = e => {
-
-        if (e.target.status !== 200) return;
-
-        let entries = [];
-
-        e.target.response.forEach(each => {
-            entries.push({
-                [each.qid]: each.lad_name
-            });
-        });
-
-        const locale_dropdown_2 = _xyz.utils.dropdownCustom({
-            entries: entries,
-            singleSelect: true,
-            placeholder: 'Select Local Authority District',
-            callback: e => {
-
-                e.stopPropagation();
-
-                document.querySelector('.gazetteer input').value = '';
-
-                selectArea(_xyz, {
-                    layer: layer.key,
-                    table: layer.table,
-                    id: e.target.dataset.field
-                });
-
-                locale_dropdown_2.querySelector('.head').textContent = e.target.textContent;
-            }
-        });
-
-        document.getElementById(dropdown_2_id).appendChild(locale_dropdown_2);
-    }
-
-    _xhr.send();
-}
-
-function createConstituencyList(_xyz){
-
-    let layer = _xyz.layers.list["Constituencies"];
-
-    layer.filter = {};
-    layer.filter.current = {};
-    layer.filter.current["regioncode"] = {};
-    layer.filter.current["regioncode"].match = e.target.dataset.field;
-
-    const _xhr = new XMLHttpRequest();
-
-    _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
-        locale: _xyz.workspace.locale.key,
-        layer: "Constituencies",
-        table: "Overview",
-        orderby: "constituency_name",
-        filter: JSON.stringify(layer.filter.current),
-        token: _xyz.token
-    }));
-
-    _xhr.setRequestHeader('Content-Type', 'application/json');
-    _xhr.responseType = 'json';
-
-    _xyz.onload = e => {
-
-        if (e.target.status !== 200) return;
-
-        let entries = [];
-
-        e.target.response.forEach(each => {
-            entries.push({
-                [each.qid]: each.constituency_name
-            });
-        });
-
-        const locale_dropdown_3 = _xyz.utils.dropdownCustom({
-            entries: entries,
-            singleSelect: true,
-            placeholder: 'Select Constituency',
-            callback: e => {
-
-                e.stopPropagation();
-                document.querySelector('.gazetteer input').value = '';
-
-                selectArea(_xyz, {
-                    layer: layer.key,
-                    table: layer.table,
-                    id: e.target.dataset.field
-                });
-
-                locale_dropdown_3.querySelector('.head').textContent = e.target.textContent;
-            }
-        });
-        document.getElementById(dropdown_3_id).appendChild(locale_dropdown_3);
-    }
-    _xhr.send();
-}
-
 function createSecondaryList(_e, _xyz, params){
-    /*
-    params = {
-        layer: "Local Authority",
-        table: "Overview",
-        label: "lad_name",
-        placeholder: "Select Local Authority District",
-        target_id: dropdown_2_id
-    }
-    */
-
 
     document.getElementById(params.target_id).innerHTML = '';
 
@@ -289,8 +98,6 @@ function createSecondaryList(_e, _xyz, params){
 
     _xhr.onload = e => {
 
-        console.log(e.target.response);
-
         if (e.target.status !== 200) return;
 
         let entries = [];
@@ -300,8 +107,6 @@ function createSecondaryList(_e, _xyz, params){
                 [each.qid]: each[params.label]
             });
         });
-
-        console.log(entries);
 
         const locale_dropdown = _xyz.utils.dropdownCustom({
             entries: entries,
@@ -322,10 +127,6 @@ function createSecondaryList(_e, _xyz, params){
                 locale_dropdown.querySelector('.head').textContent = __e.target.textContent;
             }
         });
-
-        console.log(locale_dropdown);
-
-        console.log(document.getElementById(params.target_id));
 
         document.getElementById(params.target_id).appendChild(locale_dropdown);
     }
