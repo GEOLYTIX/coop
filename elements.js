6477c72ef4bc156742580c1c7ce2cb64f76d7de0
Agata Brok <agatbr@gmail.com>
2019-11-13 10:12:16 +0000

    Added 3rd dropdown.

--- elements.js
+++ elements.js
@@ -20,7 +20,8 @@ function createRegionList(_xyz) { // create region dropdown
 
     // selectors
     const dropdown_1_id = 'xyz_locale_dropdown_1',
-          dropdown_2_id = 'xyz_locale_dropdown_2';
+          dropdown_2_id = 'xyz_locale_dropdown_2',
+          dropdown_3_id = 'xyz_locale_dropdown_3';
 
     document.getElementById(dropdown_1_id).innerHTML = '';
     document.getElementById(dropdown_2_id).innerHTML = '';
@@ -51,9 +52,25 @@ function createRegionList(_xyz) { // create region dropdown
 
             locale_dropdown_1.querySelector('.head').textContent = e.target.textContent;
 
+            createSecondaryList(_xyz, {
+                layer: "Local Authority",
+                table: "Overview",
+                label: "lad_name",
+                placeholder: "Select Local Authority District",
+                target_id: dropdown_2_id
+            });
 
+            createSecondaryList(_xyz, {
+                layer: "Constituencies",
+                table: "Overview",
+                label: "constituency_name",
+                placeholder: "Select Constituency",
+                target_id: dropdown_3_id
+            });
 
-            let layer = _xyz.layers.list["Local Authority"];
+            //createLADList(_xyz);
+
+            /*let layer = _xyz.layers.list["Local Authority"];
 
             layer.filter = {};
             layer.filter.current = {};
@@ -105,11 +122,203 @@ function createRegionList(_xyz) { // create region dropdown
                         locale_dropdown_2.querySelector('.head').textContent = e.target.textContent;
                     }
                 });
+
                 document.getElementById(dropdown_2_id).appendChild(locale_dropdown_2);
+
             }
-            _xhr.send();
+            _xhr.send();*/
         }
     });
 
     return locale_dropdown_1;
+}
+
+function createLADList(_xyz){
+
+    let layer = _xyz.layers.list["Local Authority"];
+
+    layer.filter = {};
+    layer.filter.current = {};
+    layer.filter.current["regioncode"] = {};
+    layer.filter.current["regioncode"].match = e.target.dataset.field;
+
+    const _xhr = new XMLHttpRequest();
+
+    _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
+        locale: _xyz.workspace.locale.key,
+        layer: "Local Authority",
+        table: "Overview",
+        orderby: "lad_name",
+        filter: JSON.stringify(layer.filter.current),
+        token: _xyz.token
+    }));
+
+    _xhr.setRequestHeader('Content-Type', 'application/json');
+    _xhr.responseType = 'json';
+
+    _xyz.onload = e => {
+
+        if (e.target.status !== 200) return;
+
+        let entries = [];
+
+        e.target.response.forEach(each => {
+            entries.push({
+                [each.qid]: each.lad_name
+            });
+        });
+
+        const locale_dropdown_2 = _xyz.utils.dropdownCustom({
+            entries: entries,
+            singleSelect: true,
+            placeholder: 'Select Local Authority District',
+            callback: e => {
+
+                e.stopPropagation();
+
+                document.querySelector('.gazetteer input').value = '';
+
+                selectArea(_xyz, {
+                    layer: layer.key,
+                    table: layer.table,
+                    id: e.target.dataset.field
+                });
+
+                locale_dropdown_2.querySelector('.head').textContent = e.target.textContent;
+            }
+        });
+
+        document.getElementById(dropdown_2_id).appendChild(locale_dropdown_2);
+    }
+
+    _xhr.send();
+}
+
+function createConstituencyList(_xyz){
+
+    let layer = _xyz.layers.list["Constituencies"];
+
+    layer.filter = {};
+    layer.filter.current = {};
+    layer.filter.current["regioncode"] = {};
+    layer.filter.current["regioncode"].match = e.target.dataset.field;
+
+    const _xhr = new XMLHttpRequest();
+
+    _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
+        locale: _xyz.workspace.locale.key,
+        layer: "Constituencies",
+        table: "Overview",
+        orderby: "constituency_name",
+        filter: JSON.stringify(layer.filter.current),
+        token: _xyz.token
+    }));
+
+    _xhr.setRequestHeader('Content-Type', 'application/json');
+    _xhr.responseType = 'json';
+
+    _xyz.onload = e => {
+
+        if (e.target.status !== 200) return;
+
+        let entries = [];
+
+        e.target.response.forEach(each => {
+            entries.push({
+                [each.qid]: each.constituency_name
+            });
+        });
+
+        const locale_dropdown_3 = _xyz.utils.dropdownCustom({
+            entries: entries,
+            singleSelect: true,
+            placeholder: 'Select Constituency',
+            callback: e => {
+
+                e.stopPropagation();
+                document.querySelector('.gazetteer input').value = '';
+
+                selectArea(_xyz, {
+                    layer: layer.key,
+                    table: layer.table,
+                    id: e.target.dataset.field
+                });
+
+                locale_dropdown_3.querySelector('.head').textContent = e.target.textContent;
+            }
+        });
+        document.getElementById(dropdown_3_id).appendChild(locale_dropdown_3);
+    }
+    _xhr.send();
+}
+
+function createSecondaryList(_xyz, params){
+
+    /*
+    params = {
+        layer: "Local Authority",
+        table: "Overview",
+        label: "lad_name",
+        placeholder: "Select Local Authority District",
+        target_id: dropdown_2_id
+    }
+    */
+
+    let layer = _xyz.layers.list[params.layer];
+
+    layer.filter = {};
+    layer.filter.current = {};
+    layer.filter.current["regioncode"] = {};
+    layer.filter.current["regioncode"].match = e.target.dataset.field;
+
+    const _xhr = new XMLHttpRequest();
+
+    _xhr.open('GET', _xyz.host + '/api/layer/table?' + _xyz.utils.paramString({
+        locale: _xyz.workspace.locale.key,
+        layer: params.layer,
+        table: params.table,
+        orderby: params.label,
+        filter: JSON.stringify(layer.filter.current),
+        token: _xyz.token
+    }));
+
+    _xhr.setRequestHeader('Content-Type', 'application/json');
+    _xhr.responseType = 'json';
+
+    _xyz.onload = e => {
+
+        if (e.target.status !== 200) return;
+
+        let entries = [];
+
+        e.target.response.forEach(each => {
+            entries.push({
+                [each.qid]: each[params.label]
+            });
+        });
+
+        const locale_dropdown = _xyz.utils.dropdownCustom({
+            entries: entries,
+            singleSelect: true,
+            placeholder: params.placeholder,
+            callback: e => {
+
+                e.stopPropagation();
+
+                document.querySelector('.gazetteer input').value = '';
+
+                selectArea(_xyz, {
+                    layer: layer.key,
+                    table: layer.table,
+                    id: e.target.dataset.field
+                });
+
+                locale_dropdown.querySelector('.head').textContent = e.target.textContent;
+            }
+        });
+
+        document.getElementById(params.target_id).appendChild(locale_dropdown);
+    }
+
+    _xhr.send();
 }
