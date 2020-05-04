c6cb08784e9756e1e9c75b2a11d17e32105a24e5
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-04 15:21:51 +0100

    Create community_wellbeing-index.sql

---
+++ query/community_wellbeing-index.sql
@@ -0,0 +1,13 @@
+SELECT
+  dd_name,
+  ind_wellb * 100 AS ind_wellb,
+  indeduca * 100 AS indeduca,
+  indhealt * 100 AS indhealt,
+  indecono * 100 AS indecono,
+  indcultu * 100 AS indcultu,
+  indtrans * 100 AS indtrans,
+  indhousi * 100 AS indhousi,
+  indrelat * 100 AS indrelat,
+  indequal * 100 AS indequal,
+  indvoice * 100 AS indvoice
+FROM coop.uk_coop_restrict_wellbeing LIMIT 99;
