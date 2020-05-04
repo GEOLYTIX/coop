c25c52d6607f1ee24c36144baf2f35875e9fc233
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-04 15:30:06 +0100

    Update community_wellbeing_index.sql

--- query/community_wellbeing_index.sql
+++ query/community_wellbeing_index.sql
@@ -1,15 +1,15 @@
 SELECT
   id,
   dd_name,
-  ind_wellb * 100 AS ind_wellb,
-  indeduca * 100 AS indeduca,
-  indhealt * 100 AS indhealt,
-  indecono * 100 AS indecono,
-  indcultu * 100 AS indcultu,
-  indtrans * 100 AS indtrans,
-  indhousi * 100 AS indhousi,
-  indrelat * 100 AS indrelat,
-  indequal * 100 AS indequal,
-  indvoice * 100 AS indvoice
+  round(ind_wellb * 100) AS ind_wellb,
+  round(indeduca * 100) AS indeduca,
+  round(indhealt * 100) AS indhealt,
+  round(indecono * 100) AS indecono,
+  round(indcultu * 100) AS indcultu,
+  round(indtrans * 100) AS indtrans,
+  round(indhousi * 100) AS indhousi,
+  round(indrelat * 100) AS indrelat,
+  round(indequal * 100) AS indequal,
+  round(indvoice * 100) AS indvoice
 FROM coop.uk_coop_restrict_wellbeing
 WHERE true ${viewport} ${filter} FETCH FIRST 99 ROW ONLY;
