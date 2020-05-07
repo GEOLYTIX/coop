7e8ddb205af1ebc08fcad6224fec30833782cd3b
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-07 16:22:45 +0100

    Update get_lad_from_region.sql

--- query/get_lad_from_region.sql
+++ query/get_lad_from_region.sql
@@ -1,3 +1,5 @@
-SELECT lad_name 
-FROM coop.vw_uk_glx_geodata_admin_lad 
-WHERE regionname = '${region}'
+SELECT ARRAY(
+  SELECT lad_name 
+  FROM coop.vw_uk_glx_geodata_admin_lad 
+  WHERE regionname = '${region}'
+);
