4682c9bcdc2b94b6230ef82d3430d02d4c9ce02e
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-07 20:12:17 +0100

    Update community_wellbeing_index_compare.sql

--- query/community_wellbeing_index_compare.sql
+++ query/community_wellbeing_index_compare.sql
@@ -1,36 +1,38 @@
 SELECT
-  id,
-  dd_name,
-  '#FFD60C' AS backgroundcolour,
-  round(ind_wellb * 100) AS ind_wellb,
-  round(indeduca * 100) AS indeduca,
-  round(indhealt * 100) AS indhealt,
-  round(indecono * 100) AS indecono,
-  round(indcultu * 100) AS indcultu,
-  round(indtrans * 100) AS indtrans,
-  round(indhousi * 100) AS indhousi,
-  round(indrelat * 100) AS indrelat,
-  round(indequal * 100) AS indequal,
-  round(indvoice * 100) AS indvoice
+ id,
+ dd_name,
+ '#FFD60C' AS backgroundcolour,
+ round(ind_wellb * 100) AS ind_wellb,
+ round(indeduca * 100) AS indeduca,
+ round(indhealt * 100) AS indhealt,
+ round(indecono * 100) AS indecono,
+ round(indcultu * 100) AS indcultu,
+ round(indtrans * 100) AS indtrans,
+ round(indhousi * 100) AS indhousi,
+ round(indrelat * 100) AS indrelat,
+ round(indequal * 100) AS indequal,
+ round(indvoice * 100) AS indvoice
 FROM coop.uk_coop_restrict_wellbeing
-WHERE dd_name LIKE '${loc}'
+WHERE dd_name LIKE 'Penkridge'
 
 UNION ALL
 
-SELECT
-  id,
-  dd_name,
-  null AS backgroundcolour,
-  round(ind_wellb * 100) AS ind_wellb,
-  round(indeduca * 100) AS indeduca,
-  round(indhealt * 100) AS indhealt,
-  round(indecono * 100) AS indecono,
-  round(indcultu * 100) AS indcultu,
-  round(indtrans * 100) AS indtrans,
-  round(indhousi * 100) AS indhousi,
-  round(indrelat * 100) AS indrelat,
-  round(indequal * 100) AS indequal,
-  round(indvoice * 100) AS indvoice
-FROM coop.uk_coop_restrict_wellbeing
-ORDER BY geom_p_4326 <-> st_setsrid(st_point(${lng}, ${lat}), 4326)
-LIMIT 9
+SELECT y.*
+from (select st_setsrid(st_point(${lng}, ${lat}), 4326) geom_p) a
+cross join lateral
+( select  id,
+ dd_name,
+ null AS backgroundcolour,
+ round(ind_wellb * 100) AS ind_wellb,
+ round(indeduca * 100) AS indeduca,
+ round(indhealt * 100) AS indhealt,
+ round(indecono * 100) AS indecono,
+ round(indcultu * 100) AS indcultu,
+ round(indtrans * 100) AS indtrans,
+ round(indhousi * 100) AS indhousi,
+ round(indrelat * 100) AS indrelat,
+ round(indequal * 100) AS indequal,
+ round(indvoice * 100) AS indvoice
+FROM coop.uk_coop_restrict_wellbeing w
+ORDER BY w.geom_p_4326 <-> a.geom_p
+LIMIT 9) y
