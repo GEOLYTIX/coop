6904e7ef471a9e88d8b9dbe18931a02e14117d5f
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-07 17:08:40 +0100

    Update get_constituency_from_region.sql

--- query/get_constituency_from_region.sql
+++ query/get_constituency_from_region.sql
@@ -1,6 +1,6 @@
 
 SELECT ARRAY(
   SELECT constituency_name 
-  FROM coop.vw_uk_glx_geodata_admin_lad 
+  FROM coop.uk_open_admin_constituencies_regions 
   WHERE regionname = '${region}'
 ) AS constituency_name;
