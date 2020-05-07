0778cdf7756d56e03ac6f880ca366539786c23f9
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-07 19:31:56 +0100

    Update community_wellbeing_index_compare.sql

--- query/community_wellbeing_index_compare.sql
+++ query/community_wellbeing_index_compare.sql
@@ -32,15 +32,5 @@ SELECT
   round(indequal * 100) AS indequal,
   round(indvoice * 100) AS indvoice
 FROM coop.uk_coop_restrict_wellbeing
-WHERE true 
-    AND ST_DWithin(
-      ST_Transform(
-        ST_MakeEnvelope(
-          -28850.673877445377,
-          6680822.621280611,
-          6446.8449202818865,
-          6737126.468338449,
-          3857
-        )
-      ,3857),
-      geom_3857, 0.00001)  FETCH FIRST 9 ROW ONLY;
+ORDER BY geom_p_4326 <-> st_setsrid(st_point(${lng}, ${lat}), 4326)
+LIMIT 9
