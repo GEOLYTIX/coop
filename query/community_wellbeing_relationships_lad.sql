42fc067542a75b39fe7351faafdaf7e269f38e6e
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-14 09:57:44 +0100

    Update community_wellbeing_relationships_lad.sql

--- query/community_wellbeing_relationships_lad.sql
+++ query/community_wellbeing_relationships_lad.sql
@@ -48,4 +48,4 @@ SELECT
   round(voi_coop_member_engagement * 100) AS voi_coop_member_engagement,
   round(voi_signing_petitions * 100) AS voi_signing_petitions
 FROM coop.uk_coop_restrict_wellbeing
-WHERE lad_name = '${lad}'
+WHERE lad_name = '${lad}';
