850b519abc5aac323d297ad0180a6dc0f9ff7876
Agata Brok <agatbr@gmail.com>
2020-05-15 12:34:33 +0100

    Update query.

--- query/community_wellbeing_index_constituency.sql
+++ query/community_wellbeing_index_constituency.sql
@@ -1,35 +1,18 @@
-SELECT
- id,
- dd_name,
- '#FFD60C' AS backgroundcolour,
- round(ind_wellb * 100) AS ind_wellb,
- round(indeduca * 100) AS indeduca,
- round(indhealt * 100) AS indhealt,
- round(indecono * 100) AS indecono,
- round(indcultu * 100) AS indcultu,
- round(indtrans * 100) AS indtrans,
- round(indhousi * 100) AS indhousi,
- round(indrelat * 100) AS indrelat,
- round(indequal * 100) AS indequal,
- round(indvoice * 100) AS indvoice
-FROM coop.uk_coop_restrict_wellbeing
-WHERE dd_name = '${loc}'
-
-UNION ALL
-
-SELECT 
-  id,
-  dd_name,
-  null AS backgroundcolour,
-  round(ind_wellb * 100) AS ind_wellb,
-  round(indeduca * 100) AS indeduca,
-  round(indhealt * 100) AS indhealt,
-  round(indecono * 100) AS indecono,
-  round(indcultu * 100) AS indcultu,
-  round(indtrans * 100) AS indtrans,
-  round(indhousi * 100) AS indhousi,
-  round(indrelat * 100) AS indrelat,
-  round(indequal * 100) AS indequal,
-  round(indvoice * 100) AS indvoice
-FROM coop.uk_coop_restrict_wellbeing
-WHERE id = '${id}'
+SELECT a.id,
+       a.dd_name,
+       round(a.ind_wellb * 100) AS               ind_wellb,
+       round(a.indeduca * 100)  AS               indeduca,
+       round(a.indhealt * 100)  AS               indhealt,
+       round(a.indecono * 100)  AS               indecono,
+       round(a.indcultu * 100)  AS               indcultu,
+       round(a.indtrans * 100)  AS               indtrans,
+       round(a.indhousi * 100)  AS               indhousi,
+       round(a.indrelat * 100)  AS               indrelat,
+       round(a.indequal * 100)  AS               indequal,
+       round(a.indvoice * 100)  AS               indvoice
+    FROM
+       coop.uk_coop_restrict_wellbeing           a,
+       coop.uk_open_admin_constituencies_regions b
+    where
+       ST_Intersects(b.geom_4326, a.geom_p_4326) AND b.id = ${id} 
+       order by dd_name limit 100;
