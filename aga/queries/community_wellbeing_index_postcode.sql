278e6348e983d7c54248f429c8c94b22d0d88b17
Agata Brok <agatbr@gmail.com>
2020-05-22 10:59:23 +0100

    Templates.

--- aga/queries/community_wellbeing_index_postcode.sql
+++ aga/queries/community_wellbeing_index_postcode.sql
@@ -1,16 +1,18 @@
-SELECT 
-  id,
-  dd_name,
+SELECT
+  a.id,
+  a.dd_name,
   null AS backgroundcolour,
-  round(ind_wellb * 100) AS ind_wellb,
-  round(indeduca * 100) AS indeduca,
-  round(indhealt * 100) AS indhealt,
-  round(indecono * 100) AS indecono,
-  round(indcultu * 100) AS indcultu,
-  round(indtrans * 100) AS indtrans,
-  round(indhousi * 100) AS indhousi,
-  round(indrelat * 100) AS indrelat,
-  round(indequal * 100) AS indequal,
-  round(indvoice * 100) AS indvoice
-FROM coop.uk_coop_restrict_wellbeing
-WHERE rm_format = '${id}' ORDER BY dd_name;
+  round(a.ind_wellb * 100) AS ind_wellb,
+  round(a.indeduca * 100) AS indeduca,
+  round(a.indhealt * 100) AS indhealt,
+  round(a.indecono * 100) AS indecono,
+  round(a.indcultu * 100) AS indcultu,
+  round(a.indtrans * 100) AS indtrans,
+  round(a.indhousi * 100) AS indhousi,
+  round(a.indrelat * 100) AS indrelat,
+  round(a.indequal * 100) AS indequal,
+  round(a.indvoice * 100) AS indvoice
+FROM coop.uk_coop_restrict_wellbeing a, geodata.uk_glx_geodata_postal_postcode b
+WHERE b.rm_format = '${id}'
+AND ST_INTERSECTS(a.geom_4326, b.geom_p_4326)
+ORDER BY a.dd_name;
--- aga/queries/community_wellbeing_place_postcode.sql
+++ aga/queries/community_wellbeing_place_postcode.sql
@@ -1,19 +1,21 @@
 SELECT 
-  id,
-  dd_name,
+  a.id,
+  a.dd_name,
   null AS backgroundcolour,
-  round(cul_places_worship * 100) AS cul_places_worship,
-  round(cul_types_workers * 100) AS cul_types_workers,
-  round(cul_areas_leisure * 100) AS cul_areas_leisure,
-  round(cul_museums * 100) AS cul_museums,
-  round(cul_listed_buildings * 100) AS cul_listed_buildings,
-  round(tra_communication_internet * 100) AS tra_communication_internet,
-  round(tra_public_transport * 100) AS tra_public_transport,
-  round(hou_affordability * 100) AS hou_affordability,
-  round(hou_overcrowding * 100) AS hou_overcrowding,
-  round(hou_green_space * 100) AS hou_green_space,
-  round(hou_public_spaces * 100) AS hou_public_spaces,
-  round(hou_pollution * 100) AS hou_pollution,
-  round(hou_air_quality * 100) AS hou_air_quality
-FROM coop.uk_coop_restrict_wellbeing
-WHERE rm_format = '${id}' ORDER BY dd_name;
+  round(a.cul_places_worship * 100) AS cul_places_worship,
+  round(a.cul_types_workers * 100) AS cul_types_workers,
+  round(a.cul_areas_leisure * 100) AS cul_areas_leisure,
+  round(a.cul_museums * 100) AS cul_museums,
+  round(a.cul_listed_buildings * 100) AS cul_listed_buildings,
+  round(a.tra_communication_internet * 100) AS tra_communication_internet,
+  round(a.tra_public_transport * 100) AS tra_public_transport,
+  round(a.hou_affordability * 100) AS hou_affordability,
+  round(a.hou_overcrowding * 100) AS hou_overcrowding,
+  round(a.hou_green_space * 100) AS hou_green_space,
+  round(a.hou_public_spaces * 100) AS hou_public_spaces,
+  round(a.hou_pollution * 100) AS hou_pollution,
+  round(a.hou_air_quality * 100) AS hou_air_quality
+FROM coop.uk_coop_restrict_wellbeing a, geodata.uk_glx_geodata_postal_postcode b
+WHERE b.rm_format = '${id}'
+AND ST_INTERSECTS(a.geom_4326, b.geom_p_4326)
+ORDER BY a.dd_name;
